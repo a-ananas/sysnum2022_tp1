24eb62308701b86c719350c9cf941c771a465573
a-ananas <antoine.anastassiades@gmail.com>
2022-11-15 16:55:56 +0100

    commenting in the simulator

--- netlist_simulator.ml
+++ netlist_simulator.ml
@@ -5,17 +5,21 @@ let number_steps = ref (-1)
 
 exception Fail of string
 
+(* creating RAM and ROM *)
 let ram = Hashtbl.create 100
 let rom = Hashtbl.create 100
 
+(* function to initialize then env to values false *)
 let ty_to_val = function 
 	|TBit -> VBit false
 	|TBitArray n -> VBitArray (Array.make n false)
 
+(* get values of an argument in env *)
 let fetch arg env = match arg with
 	|Aconst c -> c
 	|Avar v -> Env.find v env
 
+(* binary operations on buses and bits *)
 let arr_binop bop arg1 arg2 : bool array = match bop with
 	|Or -> Array.map2 (fun a b -> a || b) arg1 arg2
 	|And -> Array.map2 (fun a b -> a && b) arg1 arg2
@@ -33,40 +37,49 @@ let eval binop arg1 arg2 = match (arg1, arg2) with
 	|((VBitArray a), (VBitArray b)) -> (VBitArray(arr_binop binop a b))
 	|_ -> raise (Fail "operation between value and array impossible... for now...")
 
+(* not operation *)
 let eval_not = function 
 	|(VBit a) -> (VBit(not (a)))
 	|(VBitArray a) -> (VBitArray(Array.map (fun x -> not x) a))
 
+(* mux CHOICE A B *)
 let mux ch a b = match (ch, a, b) with
 	|((VBit ch), _, _) -> if ch then b else a
-	|_ -> raise (Fail "weid thing in mux... no arrays for choice... yet")
+	|_ -> raise (Fail "weird thing in mux... no arrays for choice... yet")
 
+(* concatenate 2 values *)
 let concat arg1 arg2 = match (arg1, arg2) with
 	|(VBit a), (VBit b) -> (VBitArray [|a; b|])
 	|(VBit a), (VBitArray b) -> (VBitArray (Array.append [|a|] b))
 	|(VBitArray a), (VBit b) -> (VBitArray (Array.append a [|b|]))
 	|(VBitArray a), (VBitArray b) -> (VBitArray (Array.append a b))
 
+(* slice a bus *)
 let slice i j arg = 
 	if j < i 
 	then raise(Fail "Wrong indices for slicing")
 	else match arg with
 		|(VBitArray arr) -> (VBitArray (Array.sub arr i j))
-		|(VBit _) -> raise(Fail "Slicing a bit is impossible... or is it ?")
+		|(VBit v) when i = 0 && j = 1 -> (VBit v)
+        |_ -> raise(Fail "something wrong in slicing")
 
+(* select a value in a bus *)
 let select i arg = match arg with
 	|(VBitArray arr) -> (VBit(Array.get arr i))
 	|(VBit _) when i = 0 -> arg
-	|_ -> raise (Fail "thing went wrong around slice") 
+	|_ -> raise (Fail "Trying to slice wrongly") 
 
+(* function to output buses *)
 let bus_to_str bus = 
 	Array.fold_left (fun str b -> if b then str ^ "1" else str ^ "0") "" bus
 
+(* function to read value in ram giving a key *)
 let ram_read asize wsize raddr = match raddr with
 	|(VBit _) -> raise(Fail "Expecting a bus for RAM address")
 	|(VBitArray bus) when (Array.length bus) = asize -> let word = try Hashtbl.find ram bus with Not_found -> (Array.make wsize false) in (if Array.length word = wsize then VBitArray(word) else raise(Fail "Wrong word size")) 
 	|_ -> raise(Fail "Wrong address size")
 
+(* function to write value in ram giving a key (using the reg_env) *)
 let ram_write asize wsize wenb waddr wdata = match wenb with    
     |VBit true -> begin     
         match waddr with    
@@ -81,11 +94,13 @@ let ram_write asize wsize wenb waddr wdata = match wenb with
     |VBit false -> ()    
     | _ -> raise(Fail "Write enable must be a single bit")
 
+(* ROM : problem, how to initialize it ? *)
 let rom_read asize wsize raddr = match raddr with
 	|(VBit _) -> raise(Fail "Expecting a bus for ROM address")
 	|(VBitArray bus) when (Array.length bus) = asize -> let word = try Hashtbl.find rom bus with Not_found -> (Array.make wsize false) in (if Array.length word = wsize then VBitArray(word) else raise(Fail "Wrong word size")) 
 	|_ -> raise(Fail "Wrong address size")
 
+(* evaluate an equation to execute the right function with the right values *)
 let eval_eq eq env reg_env =
 	let (id, exp) = eq in 
 	match exp with
@@ -100,11 +115,13 @@ let eval_eq eq env reg_env =
 		|Eslice(i1, i2, arg) -> Env.add id (slice i1 i2 (fetch arg env)) env
 		|Eselect(i, arg) -> Env.add id (select i (fetch arg env)) env
 
+(* function to transform input in bool *)
 let char_to_bool = function
 	| '0' -> false
  	| '1' -> true
  	|  _  -> raise (Fail "Wrong input : must be 0 or 1")
 	
+(* recoded fold left for a string *)
 let str_fold_left f x a =    
   let r = ref x in    
   for i = 0 to String.length a - 1 do    
@@ -112,18 +129,22 @@ let str_fold_left f x a =
   done;    
   !r  
 
+(* transform a string into a bus *)
 let string_to_bool_arr str = str_fold_left (fun arr c -> Array.append arr [|char_to_bool c|]) [||] str
 
+(* choose between bit or bus considering the size of the input *)
 let str_to_bool str = 
  	if String.length str = 1
  	then VBit(char_to_bool str.[0])
  	else VBitArray(string_to_bool_arr str)
 
 
+(* settings the input in the environnement *)
 let rec set_inputs inp env = match inp with 
 	|[] -> env
 	|i::q -> Format.printf "%s: " i ; Format.print_flush () ; let a = read_line () in let new_env = Env.add i (str_to_bool a) env in set_inputs q new_env
 
+(* print the values of the outputs *)
 let print_vals id value = match value with
 	|VBit x -> Format.printf "%s : %d \n" id (Bool.to_int x)
 	|VBitArray arr -> Format.printf "%s;\n" (Array.fold_left (fun a b -> a ^ (string_of_int (Bool.to_int b))) (id ^ ": ") arr)
